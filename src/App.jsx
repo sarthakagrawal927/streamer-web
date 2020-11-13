22e3f0d5f107a0d83ee264cc949b5d1c9343dcb4
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-11-13 15:54:09 +0530

    added Input component, box shadows

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/' component={Home} />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
