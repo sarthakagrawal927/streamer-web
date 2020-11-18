d918fc390ee83a9adf25367e89c183d45e7d6552
unknown <sarthakagrawal927@gmail.com>
2020-11-18 20:12:35 +0530

    home axios working

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,41 +1,51 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import Button from "../Button/Button";
 import { useHistory } from "react-router-dom";
 import "./Logout.scss";
+
+import UserContext from "../../context/userContext";
+
 const Logout = ({ resetActiveTab }) => {
-    const history = useHistory();
-    const handleConfirm = () => {
-        console.log("Logout");
-        history.push("/login");
-    };
-    const handleDeny = () => {
-        console.log("Deny logout.");
-        resetActiveTab();
-    };
-    return (
-        <div className='logout-container'>
-            <div className='logout-container-heading'>Logout</div>
-            <div className='sub-container'>
-                <div className='sub-container-heading'>
-                    Do you want to logout?
-                </div>
-                <div className='sub-container-buttons'>
-                    <Button
-                        text='YES'
-                        type='submit'
-                        primary={true}
-                        onClick={handleConfirm}
-                    />
-                    <Button
-                        text='NO'
-                        type='submit'
-                        primary={false}
-                        onClick={handleDeny}
-                    />
-                </div>
-            </div>
+  const history = useHistory();
+  const { userData, setUserData } = useContext(UserContext);
+
+  const handleConfirm = () => {
+    console.log("Logout");
+    setUserData({
+      isLoggedIn: false,
+      isSuperAdmin: false,
+    });
+    localStorage.setItem("token", "");
+    console.log(localStorage.getItem("token"));
+
+    history.push("/login");
+  };
+  const handleDeny = () => {
+    console.log("Deny logout.");
+    resetActiveTab();
+  };
+  return (
+    <div className='logout-container'>
+      <div className='logout-container-heading'>Logout</div>
+      <div className='sub-container'>
+        <div className='sub-container-heading'>Do you want to logout?</div>
+        <div className='sub-container-buttons'>
+          <Button
+            text='YES'
+            type='submit'
+            primary={true}
+            onClick={handleConfirm}
+          />
+          <Button
+            text='NO'
+            type='submit'
+            primary={false}
+            onClick={handleDeny}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Logout;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,54 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 // import Sidebar from "../../components/Sidebar/Sidebar";
 import Tab from "../../components/Tab/Tab";
 import Dashboard from "../../components/Dashboard/Dashboard";
 import Logout from "../../components/Logout/Logout";
 import "./Home.scss";
+import { useHistory } from "react-router-dom";
+
+import UserContext from "../../context/userContext";
 const dashboardIcon = "/assets/svg/dashboard.svg";
 const leftArrow = "/assets/svg/left-arrow.svg";
 const Home = () => {
-    const resetActiveTab = () => {
-        setActiveItem("dashboard");
-    };
-    const [activeItem, setActiveItem] = useState("dashboard");
-    const path = window.location.pathname;
-    console.log(path);
-    return (
-        <div className='home-container'>
-            <div className='sidebar'>
-                <div className='sidebar-header'>
-                    <img
-                        className='sidebar-logo'
-                        src='/assets/images/StreamerIcon.png'
-                        alt='Streamer Logo'
-                    />
-                    <h2 className='sidebar-heading'>Streamer</h2>
-                </div>
-                <div className='sidebar-tabs'>
-                    <Tab
-                        imgSrc={dashboardIcon}
-                        text='Dashboard'
-                        link='/home'
-                        onItemClicked={() => setActiveItem("dashboard")}
-                        isActive={activeItem === "dashboard"}
-                    />
-                    <Tab
-                        imgSrc={leftArrow}
-                        text='Logout'
-                        link='/home/logout'
-                        onItemClicked={() => setActiveItem("logout")}
-                        isActive={activeItem === "logout"}
-                    />
-                </div>
-            </div>
-            <div className='content'>
-                {(activeItem === "dashboard" || "") && <Dashboard />}
-                {activeItem === "logout" && (
-                    <Logout resetActiveTab={resetActiveTab} />
-                )}
-            </div>
+  const { userData, setUserData } = useContext(UserContext);
+  const history = useHistory();
+  useEffect(() => {
+    if (!userData.isLoggedIn) history.push("/login");
+  }, []);
+  const resetActiveTab = () => {
+    setActiveItem("dashboard");
+  };
+  const [activeItem, setActiveItem] = useState("dashboard");
+  const path = window.location.pathname;
+  console.log(path);
+  return (
+    <div className='home-container'>
+      <div className='sidebar'>
+        <div className='sidebar-header'>
+          <img
+            className='sidebar-logo'
+            src='/assets/images/StreamerIcon.png'
+            alt='Streamer Logo'
+          />
+          <h2 className='sidebar-heading'>Streamer</h2>
         </div>
-    );
+        <div className='sidebar-tabs'>
+          <Tab
+            imgSrc={dashboardIcon}
+            text='Dashboard'
+            link='/home'
+            onItemClicked={() => setActiveItem("dashboard")}
+            isActive={activeItem === "dashboard"}
+          />
+          <Tab
+            imgSrc={leftArrow}
+            text='Logout'
+            link='/home/logout'
+            onItemClicked={() => setActiveItem("logout")}
+            isActive={activeItem === "logout"}
+          />
+        </div>
+      </div>
+      <div className='content'>
+        {(activeItem === "dashboard" || "") && <Dashboard />}
+        {activeItem === "logout" && <Logout resetActiveTab={resetActiveTab} />}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
